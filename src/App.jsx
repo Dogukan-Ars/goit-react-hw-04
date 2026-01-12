068d1f4ff56699cc3bb66e2c365559864cba0dd1
Dogukan-Ars <arss.dogukan@gmail.com>
2026-01-12 23:57:35 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import './App.css'
 import SearchBar from './components/SearchBar/SearchBar'
 import ImageGallery from './components/ImageGallery/ImageGallery'
+import Loader from './components/Loader/Loader'
+import ErrorMessage from './components/ErrorMessage/ErrorMessage'
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (topic) => {
+    try {
+      setImages([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchImagesWithTopic(topic);
+      setImages(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <>
-      <SearchBar />
-      <ImageGallery />
-    </>
-  )
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {images.length > 0 && <ImageGallery items={images} />}
+    </div>
+  );
 }
 
 export default App
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,20 @@
 import "./SearchBar.module.css"
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+    // src/components/SearchForm.jsx
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const form = evt.target;
+        const topic = form.elements.topic.value;
+        onSearch(topic);
+        form.reset();
+    };
+
     return (
         <>
             <header>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
                         className="input"
                         type="text"
